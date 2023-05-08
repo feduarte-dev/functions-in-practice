3b21d60d30a8ae3d2d4e1a5a3f2ecb26b4ea1b9f
Felipe Duarte <feduarte.dev@gmail.com>
2023-05-08 16:06:04 -0300

    first challenge complete

--- src/functions.js
+++ src/functions.js
@@ -1,15 +1,18 @@
 // 1 - Crie a função ligarDesligar, que ligue e desligue um motor de um carro.
 
+const ligarDesligar = status => { status === 'ligado' ? status = 'desligado' : status = 'ligado';
+  console.log(`O motor está ${status}`)
+  return status;
+};
 
-// 2 - Crie a função circleArea, que calcule a área de um círculo. 
 
+// 2 - Crie a função circleArea, que calcule a área de um círculo.
 
 // 3 - Crie a função longestWord, que receba uma frase como parâmetro e retorne a maior palavra da frase.
 
-
 // Não modifique as linhas abaixo
 module.exports = {
-  ligarDesligar: typeof ligarDesligar === 'function' ? ligarDesligar : (() => {}),
-  circleArea: typeof circleArea === 'function' ? circleArea : (() => {}),
-  longestWord: typeof longestWord === 'function' ? longestWord : (() => {}),
+  ligarDesligar: typeof ligarDesligar === 'function' ? ligarDesligar : () => {},
+  circleArea: typeof circleArea === 'function' ? circleArea : () => {},
+  longestWord: typeof longestWord === 'function' ? longestWord : () => {},
 };
