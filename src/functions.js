79246c808e9bc6343f83baefd6c8e8562b44d209
Felipe Duarte <feduarte.dev@gmail.com>
2023-05-08 18:53:18 -0300

    fixed second exercise

--- src/functions.js
+++ src/functions.js
@@ -9,13 +9,9 @@ const ligarDesligar = (status) => {
 // 2 - Crie a função circleArea, que calcule a área de um círculo.
 
 const circleArea = (raio) => {
-  if (typeof raio !== 'number') {
-    return 'O parâmetro radius deve ser um número';
-  }
-  return `Essa é a área do círculo: ${3.14 * (raio * raio)}`;
+return typeof raio !== 'number' ?  `O parâmetro radius deve ser um número` :  `Essa é a área do círculo: ${3.14 * (raio * raio)}`;
 };
 
-//  typeof raio !== 'number' ? return `O parâmetro radius deve ser um número` : return `Essa é a área do círculo: ${3.14 * (raio * raio)}`; por que não funciona?
 
 // 3 - Crie a função longestWord, que receba uma frase como parâmetro e retorne a maior palavra da frase.
 
