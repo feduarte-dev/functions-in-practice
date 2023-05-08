c6e17b7edeb4be8232ef50d0cf333fa8379cdc5a
Felipe Duarte <feduarte.dev@gmail.com>
2023-05-08 16:34:12 -0300

    third exercise complete

--- src/functions.js
+++ src/functions.js
@@ -15,8 +15,22 @@ const circleArea = (raio) => {
     return `Essa é a área do círculo: ${3.14 * (raio * raio)}`;
   }
 };
+//typeof raio !== 'number' ? return `O parâmetro radius deve ser um número` : return `Essa é a área do círculo: ${3.14 * (raio * raio)}`; por que não funciona?
 
 // 3 - Crie a função longestWord, que receba uma frase como parâmetro e retorne a maior palavra da frase.
+
+function longestWord(frase) {
+  const palavras = frase.split(" ");
+  let maiorPalavra = "";
+  for (let index = 0; index < palavras.length; index += 1) {
+    if (palavras[index].length > maiorPalavra.length) {
+      maiorPalavra = palavras[index];
+    }
+  }
+
+ return maiorPalavra;
+}
+
 
 // Não modifique as linhas abaixo
 module.exports = {
