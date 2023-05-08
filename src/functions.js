662107a0b8fde898f172b059060a2e473dab467c
Felipe Duarte <feduarte.dev@gmail.com>
2023-05-08 16:30:58 -0300

    second exercise complete

--- src/functions.js
+++ src/functions.js
@@ -1,12 +1,20 @@
 // 1 - Crie a função ligarDesligar, que ligue e desligue um motor de um carro.
 
-const ligarDesligar = status => { status === 'ligado' ? status = 'desligado' : status = 'ligado';
-  console.log(`O motor está ${status}`)
+const ligarDesligar = (status) => {
+  status === 'ligado' ? (status = 'desligado') : (status = 'ligado');
+  console.log(`O motor está ${status}`);
   return status;
 };
 
-
 // 2 - Crie a função circleArea, que calcule a área de um círculo.
+
+const circleArea = (raio) => {
+  if (typeof raio !== 'number') {
+    return `O parâmetro radius deve ser um número`;
+  } else {
+    return `Essa é a área do círculo: ${3.14 * (raio * raio)}`;
+  }
+};
 
 // 3 - Crie a função longestWord, que receba uma frase como parâmetro e retorne a maior palavra da frase.
 
